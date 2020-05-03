93c0b39fcb38465aff088b2668d81da9c43ac645
visualGravitySense <helikeel@gmail.com>
2020-05-03 20:03:21 +0300

    final version Instagram bot commit 3

--- getpages.py
+++ getpages.py
@@ -32,22 +32,35 @@ class Getpages:
         flw_btn.click()
         time.sleep(3)
         self.popup = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]')))
-        for h in range (10):
+        for h in range (11):
             time.sleep(1)
             print('scrolling')
             print(h)
             print('arguments[0].scrollTop = arguments[0].scrollHeight/{}'.format(str(11-h)))
             self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight/{}'.format(str(11-h)), self.popup)
+            if h == 5:
+                break
         for i in range(15):
             time.sleep(2)
             self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', self.popup)
         self.popup = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]')))
         b_popup = b(self.popup.get_attribute('innerHTML'), 'html.parser')
         for p in b_popup.findAll('li', {'class': 'wo9IH'}):
-            print(p.findAll('a', {'class': 'FPmhX notranslate _0isma '})[0])
-'''
+            print(p.findAll('a'))
+
     def is_public(self):
         try:
-            astate = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, '/html/body/div[4]/div/div[2]')))
+            astate = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'rkEop')))
             if astate.text == 'This Account is Private':
-'''
+                return False
+            else:
+                return True
+        except:
+            return True
+    def like_post(self):
+        post = self.driver.find_element_by_css_selector('#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > a')
+        html = post.get_attribute('innerHTML')
+        h = b(html, 'html.parser')
+        href = h['href']
+        self.driver.get('https://www.instagram.com' + href)
+        like_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((by.CSS_SELECTOR 'body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > svg > path')))
