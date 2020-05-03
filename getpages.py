b624fdeb78b7199097ca6747a0f3c579e3f010e0
visualGravitySense <helikeel@gmail.com>
2020-05-03 16:51:13 +0300

    final version commit 1

--- getpages.py
+++ getpages.py
@@ -27,7 +27,21 @@ class Getpages:
 
     def get_followers(self):
         print('get followers')
+        time.sleep(2)
         flw_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#react-root > section > main > div > header > section > ul > li:nth-child(2) > a')))
         flw_btn.click()
-        popup = WebDriverWait(self.driver, 10). until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]')))
-        self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', popup)
+        self.popup = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]')))
+        for i in range (10, 5, 1):
+            time.sleep(1)
+            self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight/{}'.format(h), self.popup)
+        for i in range(15):
+            time.sleep(2)
+            self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', self.popup)
+        self.popup = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]')))
+        b_popup = b(self.popup.get_attribute('innerHTML'), 'html.parser')
+        for p in b_popup.findAll('li', {'class': 'wo9IH'}):
+            print(p.findAll('a', {'class': 'FPmhX notranslate _0isma '})[0])
+    def is_public(self):
+        try:
+            astate = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, '/html/body/div[4]/div/div[2]')))
+            if astate.text == 'This Account is Private':
