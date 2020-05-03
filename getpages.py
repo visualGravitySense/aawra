ae4bc3efe8d3fc5f3a0614e9b342832e03db5814
visualGravitySense <helikeel@gmail.com>
2020-05-03 16:54:42 +0300

    final version bot commit 1.a

--- getpages.py
+++ getpages.py
@@ -31,7 +31,7 @@ class Getpages:
         flw_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#react-root > section > main > div > header > section > ul > li:nth-child(2) > a')))
         flw_btn.click()
         self.popup = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]')))
-        for i in range (10, 5, 1):
+        for h in range (10, 5, 1):
             time.sleep(1)
             self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight/{}'.format(h), self.popup)
         for i in range(15):
@@ -41,7 +41,4 @@ class Getpages:
         b_popup = b(self.popup.get_attribute('innerHTML'), 'html.parser')
         for p in b_popup.findAll('li', {'class': 'wo9IH'}):
             print(p.findAll('a', {'class': 'FPmhX notranslate _0isma '})[0])
-    def is_public(self):
-        try:
-            astate = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, '/html/body/div[4]/div/div[2]')))
-            if astate.text == 'This Account is Private':
+    
